412741e30a35b4865061abf29c978107a7282e4d
lvarnum <lvarnum@email.arizona.edu>
2020-04-18 17:47:35 -0700

    Added styles to team page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Dependencies ---------------------------------------
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,6 +11,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//-----------------------------------------------------
+
+// Arrays to hold inquirer prompts and employee objects
 var questions = [
     {
         message: "Employee name: ",
@@ -30,7 +34,10 @@ var questions = [
 ];
 
 var employees = [];
+//------------------------------------------------------
 
+// Ask user if they would like to continue entering employees and call 
+// the getInput() function if they do. Otherwise render the team page.
 function start() {
     inquirer.prompt({
         message: "Would you like to enter an employee? (y/n) ",
@@ -47,9 +54,10 @@ function start() {
         }
     });
 }
+//----------------------------------------------------------------------
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Use inquirer to gather information about the development team members,
+// create objects for each team member based on their role.
 function getInput() {
     inquirer.prompt(questions)
         .then(answers => {
@@ -80,32 +88,23 @@ function getInput() {
                     employees.push(new Engineer(name, id, email, info));
                 }
                 if (role === "manager") {
-                    employess.push(new Manager(name, id, email, info));
+                    employees.push(new Manager(name, id, email, info));
                 }
                 start();
             });
         });
 }
+//----------------------------------------------------------------------------
 
+// Write the team.html file to the output folder with the html
+// from the render function.
 function createOutput(html) {
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR);
-    }
     fs.writeFile(outputPath, html, (err) => {
         if (err) throw err;
         console.log("Team Page Rendered!");
     })
 }
+//------------------------------------------------------------
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
+// Call the starting function
 start();
